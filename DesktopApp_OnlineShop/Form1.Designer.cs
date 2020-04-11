a7c32d23f71dacad94e4f392adc99210d5bad633
Ivan <ivan199863@gmail.com>
2020-04-11 15:54:08 +0500

    Добавил основные классы

--- DesktopApp_OnlineShop/Form1.Designer.cs
+++ DesktopApp_OnlineShop/Form1.Designer.cs
@@ -28,13 +28,74 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.panel4 = new System.Windows.Forms.Panel();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Location = new System.Drawing.Point(12, 56);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(77, 56);
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// panel1
+			// 
+			this.panel1.Location = new System.Drawing.Point(12, 12);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(1240, 38);
+			this.panel1.TabIndex = 1;
+			// 
+			// panel2
+			// 
+			this.panel2.Location = new System.Drawing.Point(95, 56);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(1157, 23);
+			this.panel2.TabIndex = 2;
+			// 
+			// panel3
+			// 
+			this.panel3.Location = new System.Drawing.Point(95, 85);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(1157, 27);
+			this.panel3.TabIndex = 3;
+			// 
+			// panel4
+			// 
+			this.panel4.Location = new System.Drawing.Point(1101, 118);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(151, 511);
+			this.panel4.TabIndex = 4;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.ClientSize = new System.Drawing.Size(1264, 681);
+			this.Controls.Add(this.panel4);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.pictureBox1);
+			this.Name = "Form1";
 			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Panel panel4;
 	}
 }
 
